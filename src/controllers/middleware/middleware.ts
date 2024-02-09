2c11f5024f17149d66c2d881128d8e2237b94b23
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-02-09 15:52:02 +0100

    Removed console log

--- src/controllers/middleware/middleware.ts
+++ src/controllers/middleware/middleware.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
 export const emptyBodyCheck = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req)
     const emptyBody =
         (!req.body || Object.keys(req.body).length == 0) ?? false;
     if (emptyBody)
